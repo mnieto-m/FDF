674db7d602d5d9d7e155ea65c54480ae1bf66499
mnieto-m <majanoyieto@gmail.com>
2025-08-01 16:11:31 +0200

    dont crash with big maps

--- src/bresenham.c
+++ src/bresenham.c
@@ -6,7 +6,14 @@ static void	ft_point_ini(t_temp *p, t_node p0)
 	p->x = round(p0.w_xyz[0]);//x);
 	p->y = round(p0.w_xyz[1]);
 }
-
+int pt_in_img(t_temp p)
+{
+	if (p.x < 0 || p.x >= WIDTH)
+		return (0);
+	if (p.y < 0 || p.y >= HEIGHT)
+		return (0);
+	return (1);
+}
 static void	ft_bresh_low(t_map *data, t_node p0, t_node p1)
 {
 	t_temp	d;
@@ -24,7 +31,8 @@ static void	ft_bresh_low(t_map *data, t_node p0, t_node p1)
 	while (p.x < round(p1.w_xyz[0]))
 	{
 		//colour = ft_lerp_rgb(p0.colour, p1.colour, fabs(p0.x - p.x), d.x);
-		mlx_put_pixel(data->img, p.x, p.y, 0);//cambiar color
+		if(pt_in_img(p))
+			mlx_put_pixel(data->img, p.x, p.y, 0);//cambiar color
 		if (diff > 0)
 		{
 			p.y += yi;
@@ -50,9 +58,9 @@ static void	ft_bresh_high(t_map *data, t_node p0, t_node p1)
 	ft_point_ini(&p, p0);
 	while (p.y < round(p1.w_xyz[1]))
 	{
-	
 		//colour = ft_lerp_rgb(p0.colour, p1.colour, fabs(p0.y - p.y), d.y);
-		mlx_put_pixel(data->img, p.x, p.y, 0);//cambiar color
+		if(pt_in_img(p))
+			mlx_put_pixel(data->img, p.x, p.y, 0);//cambiar color
 		if (diff > 0)
 		{
 			p.x += xi;
